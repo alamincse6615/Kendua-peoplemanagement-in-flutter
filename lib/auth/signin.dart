0a75b2e5ab48cbab77d42c79119d65855083b390
mdosman202166 <mdosman202166@gmail.com>
2022-01-17 13:29:24 +0600

    sinup,logo

--- lib/auth/signin.dart
+++ lib/auth/signin.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kendua/auth/signup.dart';
 import 'package:kendua/dashboard/dashboard.dart';
@@ -19,121 +21,134 @@ class _SignInState extends State<SignIn> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Sign In to continue'),
-      ),
-      body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(
-              height: 20,
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * .25,
-              width: MediaQuery.of(context).size.width,
-              child: Image.asset("images/logo.png"),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * .40,
-              width: MediaQuery.of(context).size.width,
-              child: Form(
-                key: _key,
-                child: Column(
-                  children: [
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        controller: ctrlemail,
-                        validator: (val){
-                          if(val==""){
-                            return "Email Can't be empty";
-                          }else if(val!.length<13){
-                            return "invalid email";
-                          }
-                        },
-                        decoration: InputDecoration(
-                          hintText: "Enter your Email",
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: TextFormField(
-                        obscureText: passwordOff,
-                        controller: ctrlpass,
-                        validator: (val){
-                          if(val==""){
-                            return "password Can't be empty";
-                          }else if(val!.length<6){
-                            return "Password must be 6 character";
-                          }
-                        },
-                        decoration: InputDecoration(
-                          suffixIcon: GestureDetector(
-                            onTap: (){
-                              setState(() {
-                                if(passwordOff){
-                                  passwordOff = false;
-                                  passwordIcon = Icon(Icons.visibility);
-                                }
-                                else if(!passwordOff){
-                                  passwordOff = true;
-                                  passwordIcon = Icon(Icons.visibility_off);
-                                }
-                              });
-                            },
-                            child: passwordIcon,
-                          ),
-                          hintText: "Enter your password",
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(
-                        onPressed: () {
-                          signinfun(ctrlemail.text,ctrlpass.text);
-                        },
-                        child: Text("Sign In")),
-                    Center(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text("Create An Account ? "),
-                          InkWell(
-                              onTap: () {
-                                Navigator.push(
-                                    context,
-                                    MaterialPageRoute(
-                                        builder: (context) => SignUp()));
-                              },
-                              child: Text(
-                                "Sign Up",
-                                style: TextStyle(color: Colors.red),
-                              )),
-                        ],
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-          ],
+        appBar: AppBar(
+          title: Text('Sign In to continue'),
         ),
-      ),
-    );
+        body: FutureBuilder(
+          future: firebaseinit(),
+            builder:
+            (context, info) {
+          return SingleChildScrollView(
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 20,
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * .25,
+                  width: MediaQuery.of(context).size.width,
+                  child: Image.asset("images/logo.png"),
+                ),
+                Container(
+                  height: MediaQuery.of(context).size.height * .40,
+                  width: MediaQuery.of(context).size.width,
+                  child: Form(
+                    key: _key,
+                    child: Column(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            controller: ctrlemail,
+                            validator: (val) {
+                              if (val == "") {
+                                return "Email Can't be empty";
+                              } else if (val!.length < 13) {
+                                return "invalid email";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              hintText: "Enter your Email",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: TextFormField(
+                            obscureText: passwordOff,
+                            controller: ctrlpass,
+                            validator: (val) {
+                              if (val == "") {
+                                return "password Can't be empty";
+                              } else if (val!.length < 6) {
+                                return "Password must be 6 character";
+                              }
+                            },
+                            decoration: InputDecoration(
+                              suffixIcon: GestureDetector(
+                                onTap: () {
+                                  setState(() {
+                                    if (passwordOff) {
+                                      passwordOff = false;
+                                      passwordIcon = Icon(Icons.visibility);
+                                    } else if (!passwordOff) {
+                                      passwordOff = true;
+                                      passwordIcon = Icon(Icons.visibility_off);
+                                    }
+                                  });
+                                },
+                                child: passwordIcon,
+                              ),
+                              hintText: "Enter your password",
+                              border: OutlineInputBorder(),
+                            ),
+                          ),
+                        ),
+                        ElevatedButton(
+                            onPressed: () {
+                              signinfun(ctrlemail.text, ctrlpass.text);
+                            },
+                            child: Text("Sign In")),
+                        Center(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text("Create An Account ? "),
+                              InkWell(
+                                  onTap: () {
+                                    Navigator.push(
+                                        context,
+                                        MaterialPageRoute(
+                                            builder: (context) => SignUp()));
+                                  },
+                                  child: Text(
+                                    "Sign Up",
+                                    style: TextStyle(color: Colors.red),
+                                  )),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          );
+        }));
   }
-  signinfun(String email, pass){
+
+  signinfun(String email, password)async {
     var isValid = _key.currentState!.validate();
-    if(isValid){
-      Navigator.push(
-          context,
-          MaterialPageRoute(
-              builder: (context) => Dashboard()));
-      print("Communication with database");
-    }else{
+    if (isValid) {
+       FirebaseAuth auth = FirebaseAuth.instance;
+      UserCredential  userCredential = await auth.signInWithEmailAndPassword(email: email, password: password);
+
+
+      if(userCredential.user != null){
+
+        Navigator.push(
+            context, MaterialPageRoute(builder: (context) => Dashboard()));
+      }
+    } else {
       return;
     }
     _key.currentState!.save();
+  }
+
+  Future<FirebaseApp> firebaseinit() async {
+    FirebaseApp firebaseApp = await Firebase.initializeApp();
+    return firebaseApp;
   }
 }
