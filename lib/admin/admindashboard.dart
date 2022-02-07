1faf57e903b9fb942744b89984858f0a339b23f4
mdosman202166 <mdosman202166@gmail.com>
2022-02-07 22:35:07 +0600

    admin page

--- lib/admin/admindashboard.dart
+++ lib/admin/admindashboard.dart
@@ -12,83 +12,36 @@ class _AdminDashboardState extends State<AdminDashboard> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Admin Dashboard"),
-      ),
-      body: ListView.builder(
-          itemBuilder:
-         (context, index){
-           return Padding(
-             padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
-             child: Card(
-               child: Row(
-                 children: [
-                   Container(
-                     height: 120,
-                     width: MediaQuery.of(context).size.width * .30,
-                     child: Image.asset(
-                       "images/person.jpg",
-                       fit: BoxFit.fill,
-                     ),
-                   ),
-                   SizedBox(
-                     width: 15,
-                   ),
-                   Container(
-
-                       height: 120,
-                       width: MediaQuery.of(context).size.width * .60,
-                       child: Stack(
-                         children: [
-                           Container(
-
-                             height: 120,
-                             width: MediaQuery.of(context).size.width * .60,
-                             child: Column(
-                               mainAxisAlignment: MainAxisAlignment.start,
-                               children: [
-                                 Text(
-                                   "User Name",
-                                   textAlign: TextAlign.start,
-                                   style: TextStyle(
-                                       fontWeight: FontWeight.bold,
-                                       fontSize: 20,
-                                       color: Colors.blue),
-                                 ),
-                                 Text(
-                                   "designation",
-                                   textAlign: TextAlign.start,
-                                   style: TextStyle(
-                                       fontWeight: FontWeight.normal,
-                                       fontSize: 17,
-                                       color: Colors.black),
-                                 ),
-                                 Text(
-                                   "0124552365241",
-                                   textAlign: TextAlign.start,
-                                   style: TextStyle(
-                                       fontWeight: FontWeight.normal,
-                                       fontSize: 17,
-                                       color: Colors.grey),
-                                 ),
-                               ],
-                             ),
-                           ),
-                           Container(
-                             alignment: Alignment.topRight,
-                             height: 120,
-                             width: MediaQuery.of(context).size.width * .60,
-
-                           ),
-                         ],
-                       )),
-                 ],
-               ),
-             ),
-           );
-         }
-      ),
-      backgroundColor: Colors.green,
-    );
+        appBar: AppBar(
+          title: Text("Admin Dashboard"),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.fromLTRB(8, 4, 8, 4),
+          child: GridView.builder(
+              itemCount: 10,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2, crossAxisSpacing: 5, mainAxisSpacing: 5),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  child: Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Colors.deepPurple,
+                    ),
+                    alignment: Alignment.center,
+                    child: Text(
+                      "news",
+                      textAlign: TextAlign.center,
+                      style:
+                          TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                  onTap: () {
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => News()));
+                  },
+                );
+              }),
+        ));
   }
 }
--- lib/admin/news.dart
+++ lib/admin/news.dart
@@ -10,6 +10,22 @@ class News extends StatefulWidget {
 class _NewsState extends State<News> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(title: Text("news feed"),),
+
+      body: ListView.builder(
+          itemCount: 5,
+          itemBuilder: (BuildContext context,int index){
+            return ListTile(
+                leading: Icon(Icons.list),
+                trailing: Text("GFG",
+                  style: TextStyle(
+                      color: Colors.green,fontSize: 15),),
+                title:Text("List item $index")
+            );
+          }
+      ),
+
+    );
   }
 }
